5c0073b65e689b4e99da8f49d2e940cb54ee4bb4
Putro Dwi <putrodwi31@gmail.com>
2022-03-10 21:32:40 +0700

    My first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$ch = curl_init();
+
+$client_ip = $_SERVER["REMOTE_ADDR"];
+// set url 
+curl_setopt($ch, CURLOPT_URL, "https://ipinfo.io/$client_ip/org?token=fe3b1194955c48");
+
+// return the transfer as a string 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// $output contains the output string 
+$output = curl_exec($ch);
+
+// tutup curl 
+curl_close($ch);
+$nano = preg_replace("/^AS[0-9]+\sPT\s/", "", $output);
+echo $nano;
